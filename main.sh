c5a24bb365790b4adb45f391d2dd578a9474e59f
TheFreePenguin <81381555+TheFreePenguin@users.noreply.github.com>
2022-11-01 06:16:08 -0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -9,22 +9,22 @@ echo "2. Remove a package"
 echo "3. Install updates"
 echo "4. Clean up packages"
 read -i "Enter your option here "
-if [ $REPLY = 1 ] then
+if [ $REPLY = 1 ]; then
   echo "Enter the package(s) you wish to install."
   read TARGET
   pkexec apt install $TARGET
-elif [ $REPLY = 2 ] then
+elif [ $REPLY = 2 ]; then
   echo "Enter the package(s) you wish to remove."
   read TARGET
   echo "Do you wish to clean up after? [Y/n]"
   read CLEAN
-  if [ $CLEAN = n ] then
+  if [ $CLEAN = n ]; then
     pkexec apt remove $TARGET
   else
     pkexec bash cleanup.sh $TARGET
   fi
-elif [ $REPLY = 3 ] then
+elif [ $REPLY = 3 ]; then
   pkexec apt upgrade
-elif [ $REPLY = 4 ] then
+elif [ $REPLY = 4 ]; then
   pkexec apt autoremove
 fi
