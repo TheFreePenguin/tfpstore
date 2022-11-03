727d8f253cec12717fccfbbda4ef49f3cb2f671c
TheFreePenguin <81381555+TheFreePenguin@users.noreply.github.com>
2022-11-02 17:40:29 -0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,30 +1,37 @@
-echo "Welcome to the CLI frontend for APT! Press enter to begin."
+echo "Press ENTER to start TFP Apt Store v2.0."
 read
 echo "Checking for updates..."
-pkexec apt update
+set -e
+sudo apt update
 clear
 echo "Repos refreshed! Select an option."
 echo "1. Install a package"
 echo "2. Remove a package"
 echo "3. Install updates"
 echo "4. Clean up packages"
+echo "This APT Store has 500per cow powers."
 read -i "Enter your option here "
+while true; do
 if [ $REPLY = 1 ]; then
   echo "Enter the package(s) you wish to install."
   read TARGET
-  pkexec apt install $TARGET
+  sudo apt install $TARGET
 elif [ $REPLY = 2 ]; then
   echo "Enter the package(s) you wish to remove."
   read TARGET
-  echo "Do you wish to clean up after? [Y/n]"
+  echo "Do you wish to clean up after? [y/N]"
   read CLEAN
-  if [ $CLEAN = n ]; then
-    pkexec apt remove $TARGET
-  else
-    pkexec bash cleanup.sh $TARGET
+  sudo apt remove $TARGET
+  if [ $CLEAN = y ]; then
+    sudo apt autoremove
   fi
 elif [ $REPLY = 3 ]; then
-  pkexec apt upgrade
+  sudo apt upgrade
 elif [ $REPLY = 4 ]; then
-  pkexec apt autoremove
+  sudo apt autoremove
+elif [ $REPLY = 99 ]; then
+  exec echo "TFP APT Store is shutting down..."
+elif [ $REPLY = 500 ]; then
+  apt moo
 fi
+done
