ad830df8247c48d15687b9002d01fde0133e54a2
TheFreePenguin <81381555+TheFreePenguin@users.noreply.github.com>
2022-11-05 06:31:15 -0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -4,4 +4,6 @@ source /etc/os-release
 if [ ID_LIKE = "ubuntu" ] || [ ID_LIKE = "debian" ]; then
 echo "Your OS is ${ID}."
 exec bash debian.sh
+else
+echo "Your OS is not supported at this time."
 fi
