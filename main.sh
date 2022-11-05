e5139a6cf9912e9b5fa76caf89b6f839e1d9d82b
TheFreePenguin <81381555+TheFreePenguin@users.noreply.github.com>
2022-11-05 06:49:09 -0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,9 +1,18 @@
 echo "TFPStore v3.0 Beta 1"
+echo "Checking for sudo privileges..."
+if sudo true; then
+  SUDO="sudo"
+elif [ $UID = "root" ]
+then
+  SUDO=""
+else
+  exec echo "Unable to gain root privileges. Aborting..."
+fi
 echo "Detecting OS..."
 source /etc/os-release
-if [ ID_LIKE = "ubuntu" ] || [ ID_LIKE = "debian" ]; then
 echo "Your OS is ${ID}."
-exec bash debian.sh
+if [ ID_LIKE = "ubuntu" ] || [ ID_LIKE = "debian" ]; then
+  exec `echo $SUDO` bash debian.sh
 else
-echo "Your OS is not supported at this time."
+  echo "Your OS is not supported at this time."
 fi
