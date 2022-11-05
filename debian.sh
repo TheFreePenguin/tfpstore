48bb45414ef2b4479690fce6a969a5932e196bb9
TheFreePenguin <81381555+TheFreePenguin@users.noreply.github.com>
2022-11-04 21:34:41 -0700

    Create debian.sh

---
+++ debian.sh
@@ -0,0 +1,42 @@
+FIRSTTIME=true
+echo "Checking for updates..."
+set -e
+sudo apt update
+clear
+while true; do
+if [ FIRSTTIME = true ]; then
+  echo "Repos refreshed! Select an option."
+else
+  echo "Select an option."
+fi
+echo "1. Install a package"
+echo "2. Remove a package"
+echo "3. Install updates"
+echo "4. Clean up packages"
+echo "99. Exit"
+echo "This APT Store has 500per cow powers."
+read -i "Enter your option here "
+FIRSTTIME=false
+if [ $REPLY = 1 ]; then
+  echo "Enter the package(s) you wish to install."
+  read TARGET
+  sudo apt install $TARGET
+elif [ $REPLY = 2 ]; then
+  echo "Enter the package(s) you wish to remove."
+  read TARGET
+  echo "Do you wish to clean up after? [y/N]"
+  read CLEAN
+  sudo apt remove $TARGET
+  if [ $CLEAN = y ]; then
+    sudo apt autoremove
+  fi
+elif [ $REPLY = 3 ]; then
+  sudo apt upgrade
+elif [ $REPLY = 4 ]; then
+  sudo apt autoremove
+elif [ $REPLY = 99 ]; then
+  exec echo "TFP APT Store is shutting down..."
+elif [ $REPLY = 500 ]; then
+  apt moo
+fi
+done
