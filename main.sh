f8a0cfe13649b3b701af41a4c7ec269af078dba4
TheFreePenguin <81381555+TheFreePenguin@users.noreply.github.com>
2022-11-04 21:33:35 -0700

    Update main.sh
    Rest of script is now in debian.sh

--- main.sh
+++ main.sh
@@ -1,44 +1,7 @@
-echo "Press ENTER to start TFP Apt Store v2.0_03."
-read
-FIRSTTIME=true
-echo "Checking for updates..."
-set -e
-sudo apt update
-clear
-while true; do
-if [ FIRSTTIME = true ]; then
-  echo "Repos refreshed! Select an option."
-else
-  echo "Select an option."
+echo "TFPStore v3.0 Beta 1"
+echo "Detecting OS..."
+source /etc/os-release
+if [ ID_LIKE = "ubuntu" ] || [ ID_LIKE = "debian" ]; then
+echo "Your OS is ${ID}."
+exec bash debian.sh
 fi
-echo "1. Install a package"
-echo "2. Remove a package"
-echo "3. Install updates"
-echo "4. Clean up packages"
-echo "99. Exit"
-echo "This APT Store has 500per cow powers."
-read -i "Enter your option here "
-FIRSTTIME=false
-if [ $REPLY = 1 ]; then
-  echo "Enter the package(s) you wish to install."
-  read TARGET
-  sudo apt install $TARGET
-elif [ $REPLY = 2 ]; then
-  echo "Enter the package(s) you wish to remove."
-  read TARGET
-  echo "Do you wish to clean up after? [y/N]"
-  read CLEAN
-  sudo apt remove $TARGET
-  if [ $CLEAN = y ]; then
-    sudo apt autoremove
-  fi
-elif [ $REPLY = 3 ]; then
-  sudo apt upgrade
-elif [ $REPLY = 4 ]; then
-  sudo apt autoremove
-elif [ $REPLY = 99 ]; then
-  exec echo "TFP APT Store is shutting down..."
-elif [ $REPLY = 500 ]; then
-  apt moo
-fi
-done
