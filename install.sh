7a7fa8e6b764b453f2b25c458a73fe8c0da09f7a
TheFreePenguin <81381555+TheFreePenguin@users.noreply.github.com>
2022-11-22 11:22:19 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ install(){
   echo "cd /usr/local/share/tfpstore; bash main.sh" > /usr/local/bin/tfpstore
   chmod 755 /usr/local/bin/tfpstore
   echo "Creating uninstall binary"
-  echo "echo \"Uninstalling TFPstore...\"; rm -r /usr/local/share/aptstore; rm /usr/local/bin/aptstore; rm /usr/local/bin/aptstore-uninst" > /usr/local/bin/tfpstore-uninst
+  echo "echo \"Uninstalling TFPstore...\"; rm -r /usr/local/share/tfpstore; rm /usr/local/bin/tfpstore; rm /usr/local/bin/tfpstore-uninst" > /usr/local/bin/tfpstore-uninst
   chmod 755 /usr/local/bin/tfpstore-uninst
   echo "TFPstore can now be called by running \"tfpstore\" in the terminal. To uninstall TFPstore, run \"tfpstore-uninst\" as root."
 }
