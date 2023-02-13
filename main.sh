c3fb98742183653fad4136cfaf2171a92ce5e2be
TheFreePenguin <81381555+TheFreePenguin@users.noreply.github.com>
2023-02-13 12:45:55 -0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,7 +1,12 @@
-echo "TFPStore v3.1-namepatch"
+echo "TFPStore Kotry"
 echo "Checking for sudo privileges..."
+$IMJA=`whoami`
 if which doas; then
+  if which sudo; then
+  true
+  else
   doas ln -s `which doas` /usr/local/bin/sudo
+  doas chmod +x /usr/local/bin/sudo
 fi
 if sudo true; then
   SUDO="sudo"
@@ -19,7 +24,7 @@ if echo $ID_LIKE | grep ubuntu || echo $ID_LIKE | grep debian || [ $ID = "debian
 elif echo $ID_LIKE | grep fedora || echo $ID_LIKE | grep rhel || [ $ID = "fedora" ] || [ $ID = "rhel" ]; then
   exec `echo $SUDO` bash fedora.sh
 elif echo $ID_LIKE | grep arch || [ $ID = "arch" ]; then
-  exec `echo $SUDO` bash arch.sh
+  exec `echo $SUDO` bash arch.sh $IMJA
 elif echo $ID_LIKE | grep suse || [ $ID = "suse" ]; then
   exec `echo $SUDO` bash lizard.sh
 else
